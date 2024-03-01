94d6c66e02850a2b10a6fba76a7b99918cf7e8d3
I love kludge <98ger@mail.ru>
2024-03-01 15:16:00 +1200

    improve if blocks (> and < change on <= and >=)

--- timePeriodBelongs .js
+++ timePeriodBelongs .js
@@ -11,17 +11,17 @@ function timePeriodBelongs (mainPeriod, belongPeriod) {
     let belongPeriodStart = belongPeriod[0]
     let belongPeriodEnd = belongPeriod[1]
 
-    if (belongPeriodStart > mainPeriodStart && belongPeriodEnd < mainPeriodEnd) {
+    if (belongPeriodStart >= mainPeriodStart && belongPeriodEnd <= mainPeriodEnd) {
         return {result: 'belong', belongPeriod}
     } else {
-        if (belongPeriodStart > mainPeriodStart && belongPeriodEnd > mainPeriodEnd) {
+        if (belongPeriodStart >= mainPeriodStart && belongPeriodEnd >= mainPeriodEnd) {
             belongPeriod[1] = mainPeriodEnd
-            return {result: 'partly belong', reformedData: belongPeriod}
-        } else if (belongPeriodStart < mainPeriodStart && belongPeriodEnd < mainPeriodEnd) {
+            return {result: 'partly belong, end after main', reformedData: belongPeriod}
+        } else if (belongPeriodStart <= mainPeriodStart && belongPeriodEnd <= mainPeriodEnd) {
             belongPeriod[0] = mainPeriodStart
-            return {result: 'partly belong', reformedData: belongPeriod}
+            return {result: 'partly belong, start before main', reformedData: belongPeriod}
         } else {
-            return {false: 'not belong'}
+            return {result: 'not belong'}
         }
     }
 }
