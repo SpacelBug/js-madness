bc3c6bba309bf2db56c9b78df5941086dd8daab1
I love kludge <98ger@mail.ru>
2024-04-02 17:32:28 +1200

    add description to cursorRelativeElement

--- cursorRelativeElement.js
+++ cursorRelativeElement.js
@@ -1,3 +1,11 @@
+/**
+ * Return list of string, which get u information about cursor pos relative to element
+ *
+ * @param cursorClientX - cursor client posX (get it by event.clientX)
+ * @param cursorClientY - cursor client posY (get it by event.clientY)
+ * @param target - target DOM element
+ * @returns {*[]|string['inside']}
+ */
 function cursorRelativeElement(cursorClientX, cursorClientY, target) {
   let position = {right: false, left: false, top: false, bottom: false}
   let positionArr = []
