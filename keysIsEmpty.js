7a4b59d22e64750c66f14265910fa0b88d1747ff
I love kludge <98ger@mail.ru>
2024-04-15 16:38:27 +1200

    fix keysIsEmpty function (now work with arrays inside of object)

--- keysIsEmpty.js
+++ keysIsEmpty.js
@@ -1,14 +1,30 @@
 function keysIsEmpty(object) {
   let flag = true
 
-  for (let key in object) {
-    if (object[key] !== null) {
-      if (typeof  object[key] === 'object') {
-        flag = keysIsEmpty(object[key])
-      } else {
-        return false
+  if (!Array.isArray(object)) {
+
+    for (let key in object) {
+      if (object[key] !== null) {
+        if (typeof object[key] === 'object') {
+          flag = keysIsEmpty(object[key])
+        } else {
+          return false
+        }
       }
     }
+
+  } else {
+
+    for (let elem of object) {
+      if (elem !== null) {
+        if (typeof elem === 'object') {
+          flag = keysIsEmpty(elem)
+        } else {
+          return false
+        }
+      }
+    }
+
   }
 
   return flag
